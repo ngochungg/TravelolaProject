9b949bf812b5b71e30e4169c0b7eb9186e871736
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2022-04-30 23:59:02 +0700

    delete

--- src/main/java/com/example/travelola/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelola.repository;
-
-import com.example.travelola.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
