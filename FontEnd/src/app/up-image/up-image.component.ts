138517e5c1403cb56097874f8896b5b123995447
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2022-06-30 01:49:06 +0700

    upload image

--- FontEnd/src/app/up-image/up-image.component.ts
+++ FontEnd/src/app/up-image/up-image.component.ts
@@ -1,4 +1,8 @@
+import { HttpEvent } from '@angular/common/http';
+import { HttpRequest } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-up-image',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./up-image.component.css']
 })
 export class UpImageComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(private http: HttpClient) { }
+  selectedFile!: File;
   ngOnInit(): void {
   }
 
+  onFileSelected(event: any){
+    this.selectedFile=<File>event.target.files[0];
+  }
+
+  onUpload(){
+    const filedata =new FormData();
+    filedata.append('file', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:8080/api/auth/uploadImage/2', filedata).subscribe(res =>{console.log(res)});
+  }
 }
